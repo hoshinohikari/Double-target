e94a605e1b63081f73c3d32efe295ef64b0c58a6
hoshinohikari <zengjieliang1@gmail.com>
2019-01-04 14:17:41 +0800

    take photo

--- cap.py
+++ cap.py
@@ -5,13 +5,22 @@ cap = cv2.VideoCapture(1)
 
 width, height = cap.get(3), cap.get(4)
 
+i = 0
+
 while(1):
   ret, frame = cap.read()
+
   framel = frame[0:240, 0:320]
   framer = frame[0:240, 320:640]
+
   cv2.imshow("capl", framel)
   cv2.imshow("capr", framer)
-  if cv2.waitKey(1) & 0xFF == ord('q'):
+  if cv2.waitKey(1) & 0xFF == ord('w'):
+    cv2.imwrite("D:\\Program\\Github\\Double-target\\alphal\\%d.png"%(i), framel)
+    cv2.imwrite("D:\\Program\\Github\\Double-target\\alphar\\%d.png"%(i), framer)
+    print(i)
+    i = i + 1
+  elif cv2.waitKey(1) & 0xFF == ord('q'):
     break
 
 print(width, height)
